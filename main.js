98b60bd6ef72f0d88ac52ff285d5b7bbf346d7ee
jaimeveloso <jaimeveloso.cond@gmail.com>
2025-09-16 23:00:50 +0200

    Segunda bola y pacman redondo

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
-let objectX = 300
-let objectY = 300
-const objectSize = 20
-
 const keys = {
   ArrowLeft: false,
   ArrowRight: false,
@@ -20,14 +16,42 @@ document.addEventListener('keyup', (event) => {
   if (event.key in keys) keys[event.key] = false
 })
 
+const pacmanBall = {
+  objectX: 400,
+  objectY: 400,
+  radius: 15,
+  color: 'yellow',
+  draw: function() {
+      ctx.beginPath();
+      ctx.arc(this.objectX, this.objectY, this.radius, 0, Math.PI * 2, true);
+      ctx.closePath();
+      ctx.fillStyle = this.color;
+      ctx.fill();
+    }
+}
+
+const ball = {
+    x: 100,
+    y: 100,
+    radius: 5,
+    color: 'grey',
+    draw: function() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+        ctx.closePath();
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+  };
+
 function draw(){
     ctx.clearRect(0, 0, canvas.clientWidth, canvas.height)
-    ctx.fillStyle = "yellow"
-    ctx.fillRect(objectX, objectY, objectSize, objectSize)
-    if (keys.ArrowRight) {objectX += 1}
-    if (keys.ArrowLeft) {objectX -= 1}
-    if (keys.ArrowUp) {objectY -= 1}
-    if (keys.ArrowDown) {objectY += 1}
+    if (keys.ArrowRight) {pacmanBall.objectX += 1}
+    if (keys.ArrowLeft) {pacmanBall.objectX -= 1}
+    if (keys.ArrowUp) {pacmanBall.objectY -= 1}
+    if (keys.ArrowDown) {pacmanBall.objectY += 1}
+    pacmanBall.draw()
+    ball.draw()
     requestAnimationFrame(draw)
 }
 draw()
