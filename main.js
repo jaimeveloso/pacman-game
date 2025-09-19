77031c1452529365c3e2c46f3525e65a2de42d21
jaimeveloso <jaimeveloso.cond@gmail.com>
2025-09-19 17:36:52 +0200

    comida desaparece al colisionar

--- main.js
+++ main.js
@@ -17,13 +17,13 @@ document.addEventListener('keyup', (event) => {
 })
 
 const pacmanBall = {
-  objectX: 400,
-  objectY: 400,
+  x: 400,
+  y: 400,
   radius: 15,
   color: 'yellow',
   draw: function() {
       ctx.beginPath();
-      ctx.arc(this.objectX, this.objectY, this.radius, 0, Math.PI * 2, true);
+      ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
       ctx.closePath();
       ctx.fillStyle = this.color;
       ctx.fill();
@@ -42,17 +42,24 @@ const ball = {
         ctx.fillStyle = this.color;
         ctx.fill();
     }
-  };
+  }
 
 function draw(){
     ctx.clearRect(0, 0, canvas.clientWidth, canvas.height)
-    if (keys.ArrowRight) {pacmanBall.objectX += 1}
-    if (keys.ArrowLeft) {pacmanBall.objectX -= 1}
-    if (keys.ArrowUp) {pacmanBall.objectY -= 1}
-    if (keys.ArrowDown) {pacmanBall.objectY += 1}
+    if (keys.ArrowRight) {pacmanBall.x += 1}
+    if (keys.ArrowLeft) {pacmanBall.x -= 1}
+    if (keys.ArrowUp) {pacmanBall.y -= 1}
+    if (keys.ArrowDown) {pacmanBall.y += 1}
+
+    const distancia = Math.sqrt((pacmanBall.x - ball.x)*(pacmanBall.x - ball.x)+(pacmanBall.y - ball.y)*(pacmanBall.y - ball.y))
+    if (distancia < 15 + 5){
+      ball.color= 'transparent' //no he encontrado otra forma por hoy, hace al apaño
+    }
+    
     pacmanBall.draw()
     ball.draw()
     requestAnimationFrame(draw)
 }
 draw()
+
 
